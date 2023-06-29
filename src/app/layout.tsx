3d6c1379e4dfccb00fd03cd31745671a9510912c
davebycode <davidokoyeh@gmail.com>
2023-06-29 17:09:54 +0100

    edited

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body style={{width: '100vw', overflowX: 'hidden'}} className={inter.className}>{children}</body>
     </html>
   )
 }
